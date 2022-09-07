613bb7070602fa8483bee8f7d91374dd55fa587a
Pedro Luiz <pedroluizlsg@gmail.com>
2022-09-06 23:33:31 -0300

    Requisito 6 problema com props

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import './Table.css';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+
     return (
       <div>
         <table>
@@ -17,10 +21,47 @@ class Table extends Component {
             <th>Moeda de conversão</th>
             <th><button type="button">Editar/Excluir</button></th>
           </tr>
+
+          {
+            expenses.map((x) => (
+              <tr key={ x.id }>
+                <td>{x.description}</td>
+                <td>{x.tag}</td>
+                <td>{x.method}</td>
+                <td>{x.value}</td>
+                <td>{x.currency}</td>
+                <td>
+                  {
+                    parseFloat(x.exchangeRates[x.currency].ask).toFixed(2)
+                  }
+                </td>
+                <td>
+                  {
+                    parseFloat((x.value) * x.exchangeRates[x.currency].ask).toFixed(2)
+                  }
+                </td>
+                <td>Real</td>
+
+              </tr>
+            ))
+          }
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({ expenses: state.wallet.expenses });
+
+Table.propTypes = {
+  expenses: PropTypes.shape({
+    id: PropTypes.number,
+    description: PropTypes.string,
+    method: PropTypes.string,
+    value: PropTypes.number,
+    currency: PropTypes.string,
+    exchangeRates: PropTypes.arrayOf({}),
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
